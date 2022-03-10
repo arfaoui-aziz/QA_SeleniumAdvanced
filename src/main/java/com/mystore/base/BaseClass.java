e4bd0528ce7858124109dbb049e7797b296e928a
arfaoui-aziz <mohamedaziz.arfaoui@esprit.tn>
2022-03-10 11:38:38 +0100

    feat: load Configuration

--- src/main/java/com/mystore/base/BaseClass.java
+++ src/main/java/com/mystore/base/BaseClass.java
@@ -1,4 +1,29 @@
 package com.mystore.base;
 
+
+import org.testng.annotations.BeforeSuite;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class BaseClass {
+
+    public static Properties props;
+
+    @BeforeSuite()
+    public void loadConfig() {
+
+        try {
+            props = new Properties();
+            FileInputStream ip = new FileInputStream(
+                    System.getProperty("user.dir") + "\\Configuration\\config.properties");
+            props.load(ip);
+            System.out.println(props);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
